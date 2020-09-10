4417cafd5bae12ca42ceaf629571ed69ce5e933c
Weilin Shi <934587911@qq.com>
2020-09-10 22:05:57 +0800

    castle layout

---
+++ nodes/actor.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+export var actorName = "Unnamed"
+export var group = 0
+export var move = 5
+export var attackRange = 1
+
+export var attackPower = 24
+
+export var maxHP = 100
+export var HP = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
